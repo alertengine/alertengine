c1b5240b5019fd90f34007c2b5e734367e0f4e82
murat.oksuzer <murat.oksuzer@simsoft.com.tr>
2018-12-24 14:29:34 +0300

    some configurations added

--- src/test/java/com/pmobile/alertengine/H2IntegrationTestConfig.java
+++ src/test/java/com/pmobile/alertengine/H2IntegrationTestConfig.java
@@ -4,16 +4,17 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(
-        classes = H2IntegrationTestConfig.class)
+@SpringBootTest(classes = H2IntegrationTestConfig.class)
 @TestPropertySource(
         locations = "classpath:test.properties")
 @ComponentScan("com.pmobile.alertengine")
 @EntityScan("com.pmobile.alertengine")
+@IfProfileValue(name = "targetPlatform", value = "test")
 public class H2IntegrationTestConfig {
 
 
